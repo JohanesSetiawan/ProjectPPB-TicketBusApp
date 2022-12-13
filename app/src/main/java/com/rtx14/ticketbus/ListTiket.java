c2cb40c67881be3143e8228f824051ffa683468b
Johanes14 <setiawanjohanes41@gmail.com>
2022-12-14 00:11:59 +0700

    Update Version: 0.3.1 - TicketBusApp (Added List Tiket)

---
+++ app/src/main/java/com/rtx14/ticketbus/ListTiket.java
@@ -0,0 +1,14 @@
+package com.rtx14.ticketbus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListTiket extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_tiket);
+    }
+}
